2fcbf1eb3db619bc181efd7bbf5225f3d8d7e213
Max Diamandopol <max.diamandopol@intenthq.com>
2021-02-03 17:14:46 +0000

    Revert "Initial commit"
    This reverts commit 02d8b8fff7837554dbdeb16af69b99eefdfb7a70.

--- src/main/scala/dmndpl/blomo/Main.scala
+++
@@ -1,8 +0,0 @@
-package dmndpl.blomo
-
-import cats.effect.{ExitCode, IO, IOApp}
-
-object Main extends IOApp {
-  def run(args: List[String]) =
-    BlomoServer.stream[IO].compile.drain.as(ExitCode.Success)
-}
